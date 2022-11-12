85f84e52c9e45ba1b950e3ba8dee6bc53e68b3d0
Gilberto Junior <contato.gil.tulo@gmail.com>
2022-11-12 15:54:31 -0300

    🔧 Configurando dotnet-ef ao projeto

---
+++ Migrations/20221112185204_Adicionado-Produtos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aps6Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionadoProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Setor = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,15 +5,38 @@ using Aps6Api.Setores.Contexts;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerAps6();
+// builder.Services.AddCors(options =>
+// {
+//     options.AddPolicy(name: MyAllowSpecificOrigins,
+//                       builder =>
+//                       {
+//                           builder.AllowAnyHeader()
+//                                  .AllowAnyMethod()
+//                                  .AllowAnyOrigin();
+//                       });
+// });
+
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<MovimentacoesContext>(opt =>
-    opt.UseInMemoryDatabase("MovimentacoesList"));
-builder.Services.AddDbContext<ProdutosContext>(opt =>
-    opt.UseInMemoryDatabase("ProdutosList"));
-builder.Services.AddDbContext<SetoresContext>(opt =>
-    opt.UseInMemoryDatabase("SetoresList"));
+builder.Services.AddDbContext<MovimentacoesContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+
+builder.Services.AddDbContext<ProdutosContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+
+builder.Services.AddDbContext<SetoresContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+
 // builder.Services.AddSwaggerGen(c =>
 // {
 //    c.SwaggerDoc("v1", new() { Title = "Aps6Api", Version = "v1" });
@@ -24,13 +47,14 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (builder.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
-    //app.UseSwagger();
+    // app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
     //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Aps6Api v1"));
 }
 
 app.UseHttpsRedirection();
-
+// app.UseCors(MyAllowSpecificOrigins);
 app.UseAuthorization();
 
 app.MapControllers();
